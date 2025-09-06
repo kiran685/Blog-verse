802bbaab7bb2a0a30b107268d767376514f8b222
kiran685 <sskiran961@gmail.com>
2025-09-06 12:34:58 +0530

    data types

--- sample.js
+++ sample.js
@@ -9,6 +9,23 @@ console.log(num)
 let islogged= false
 console.log(islogged)
 
+let x;
+console.log(x)
+
+let y=null
+console.log(y)
 console.log(typeof name)
 console.log(typeof num)
 console.log(typeof islogged)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr=[1,2,3,4,5,6]
+console.log(arr[0])
+console.log(typeof arr)
+let person={
+    name:"Kiran",
+    age:18
+}
+    console.log(person.name),
+    console.log(typeof person)
