3bdc89fa7e195eba5f937d16efe2f645baf2ee78
kiran685 <sskiran961@gmail.com>
2025-09-06 15:53:59 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+function greet(){
+    console.log("Welcome to Dev Astra")
+}
+greet()
+
+function add(x,y){
+    return x+y;
+}
+let result=add(18,35)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+
+const multiply = function(x,y){
+    return x*y;
+}
+console.log(multiply(56,55))
+const square =(x)=> x*x;
+console.log(square(54))
+const subtract = (x,y)=>{
+    // console.log(x,y);
+    return x-y;
+    
+}
+console.log(subtract(45,20))
