fbe98c3cb519fd495ab96ef5d2c97429b88616b4
kiran685 <sskiran961@gmail.com>
2025-09-06 12:26:50 +0530

    multiple files

--- sample.js
+++ sample.js
@@ -1,1 +1,14 @@
 console.log("Sample file")
+
+let name="Kiran"
+console.log(name)
+
+let num=32
+console.log(num)
+
+let islogged= false
+console.log(islogged)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
